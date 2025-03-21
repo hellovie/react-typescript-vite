6bfda980c984fedc423273a51f5949ef0aa4bc0d
hellovie <hellovie@foxmail.com>
2025-03-21 23:05:26 +0800

    feat: disable EslintPlugin and StylelintPlugin in non-development environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,22 @@ import eslintPlugin from 'vite-plugin-eslint2'
 import stylelintPlugin from 'vite-plugin-stylelint'
 
 // https://vite.dev/config/
-export default defineConfig({
-  envDir: 'env',
-  plugins: [
-    react(),
-    eslintPlugin({
-      cache: false,
-      include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx']
-    }),
-    stylelintPlugin({
-      fix: false,
-      cache: false,
-      include: ['**/*.css', '**/*.scss'],
-    })
-  ],
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  return {
+    envDir: 'env',
+    plugins: [
+      react(),
+      isDev && eslintPlugin({
+        fix: false,
+        cache: false,
+        include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx']
+      }),
+      isDev && stylelintPlugin({
+        fix: false,
+        cache: false,
+        include: ['**/*.css', '**/*.scss']
+      })
+    ].filter(Boolean)
+  }
 })
