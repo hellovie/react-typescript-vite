35a592e3c99b4cd70f5879dc8c72d232a6fdd07d
hellovie <hellovie@foxmail.com>
2025-03-21 22:50:22 +0800

    feat: add VitePluginEslint2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint2'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        eslintPlugin({
+            cache: false,
+            include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx']
+        })
+    ],
 })
